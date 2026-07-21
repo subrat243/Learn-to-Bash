df8ba6496a4ada0cd5e3e9292319120b82337900
subrat243 <subratsamantaray43@gmail.com>
2026-07-21 22:09:09 +0530

    Update user_input.sh with additional user input examples and explanatory comments

--- Practice/user_input.sh
+++ Practice/user_input.sh
@@ -19,3 +19,18 @@ echo "Hello $name"
 # Confirm password has been received without displaying the actual password
 echo "Password received"
 
+# Read multiple inputs in a single line into separate variables ('city' and 'country')
+read -p "Enter your city and country:" city country
+echo "You live in $city, $country."
+
+echo -e "How are you doing?"
+# 'read' without specifying a variable name automatically stores the user's input in the default '$REPLY' variable
+read
+echo "You are doing $REPLY, That's great to hear."
+
+echo -e "What is your fav colours? "
+# 'read -a' reads space-separated input values directly into an array named 'colours'
+read -a colours
+# Access and print individual array elements using zero-based index syntax (${colours[0]}, ${colours[1]}, etc.)
+echo "Your most favourite colours are ${colours[0]}, ${colours[1]} and ${colours[2]}"
+
