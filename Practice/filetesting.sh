acce90d3a6799c82702e4f4600d4b54eb0154bac
subrat243 <subratsamantaray43@gmail.com>
2026-09-16 21:33:08 +0530

    Add comments to file testing practice script

--- Practice/filetesting.sh
+++ Practice/filetesting.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Set the path of the file to test.
 file="./file.txt"
 
+# Check whether the path exists.
 if [ -e "$file" ]; then
     echo "File exists."
 else
     echo "File does not exist."
 fi
 
+# Check whether the path points specifically to a regular file.
 if [ -f "$file" ]; then
     echo "Regular file exists."
 else
