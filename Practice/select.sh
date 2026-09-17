c5466a01c4ee63c57d9d2c88181e1b838ca40f8f
subrat243 <subratsamantaray43@gmail.com>
2026-09-17 21:35:35 +0530

    Document select practice script

--- Practice/select.sh
+++ Practice/select.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Set the prompt displayed by the select menu.
 PS3='Choose one option: '
 
+# Display a numbered menu and store the selected value in word.
 select word in "apple" "banana" "cherry" "date"; do
     echo "The word you selected is: $word"
 
+    # Exit after processing the first selection.
     break
 done
 
