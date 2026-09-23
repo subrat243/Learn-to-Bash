4e80247ba6b6ff9d972e105aaa6f20c7c6dcb4d5
subrat243 <subratsamantaray43@gmail.com>
2026-09-23 22:22:23 +0530

    Add comments to declare script

--- Practice/declare.sh
+++ Practice/declare.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# `declare -i` makes the variable an integer.
+# Example 1: using a fixed calculation.
 declare -i result
 
 result=10+20
-
 echo "The result of 10 + 20 is: $result"
 
+# Example 2: using command-line arguments.
 declare -i result
 
 result=$1+$2
-
-echo "The result of $1 + $2  is: $result"
+echo "The result of $1 + $2 is: $result"
