3d3f6fa5f01d6da7333d763cc031ab6e1331bd47
subrat243 <subratsamantaray43@gmail.com>
2026-09-23 22:21:11 +0530

    Add comments to let script

--- Practice/let.sh
+++ Practice/let.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Bash `let` command performs arithmetic evaluation.
+# Example 1: using fixed values.
 let result=8+2
 echo "The result of 8 + 2 is: $result"
 
+# Example 2: using command-line arguments.
 let result=$1+$2
 echo "The result of $1 + $2 is: $result"
