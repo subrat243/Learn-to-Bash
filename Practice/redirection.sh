48099f4286574d1156271feb868a95e8573df4ca
subrat243 <subratsamantaray43@gmail.com>
2026-09-24 20:47:07 +0530

    Document redirection practice steps

--- Practice/redirection.sh
+++ Practice/redirection.sh
@@ -4,12 +4,17 @@
 output_file="stdout.txt"
 error_file="stderr.txt"
 
+# The > operator sends standard output (file descriptor 1) to a file.
 printf 'This message is standard output.\n' > "$output_file"
+
+# The 2> operator sends standard error (file descriptor 2) to another file.
 ls missing-file 2> "$error_file"
 
+# Read both files so the two streams can be compared in the terminal.
 printf 'Standard output:\n'
 cat "$output_file"
 printf 'Standard error:\n'
 cat "$error_file"
 
+# Remove the temporary files created for this demonstration.
 rm -f "$output_file" "$error_file"
