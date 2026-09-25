a6e788a32c4f00d0d0ecd3a659a8077082d91f52
subrat243 <subratsamantaray43@gmail.com>
2026-09-25 22:52:09 +0530

    Add stdout append redirection example

--- Practice/redirection.sh
+++ Practice/redirection.sh
@@ -7,6 +7,9 @@ error_file="stderr.txt"
 # The > operator sends standard output (file descriptor 1) to a file.
 printf 'This message is standard output.\n' > "$output_file"
 
+# The >> operator appends standard output without replacing existing content.
+printf 'This message is appended to standard output.\n' >> "$output_file"
+
 # The 2> operator sends standard error (file descriptor 2) to another file.
 ls missing-file 2> "$error_file"
 
