a5598a3ecf1ad09d619f37a4238e49d7c4f7eb57
subrat243 <subratsamantaray43@gmail.com>
2026-09-26 20:44:30 +0530

    Add more arithmetic examples

--- Practice/arithmetic.sh
+++ Practice/arithmetic.sh
@@ -6,3 +6,11 @@ b=6
 echo "Addition: $((5 + 5))"
 
 echo "Subtraction: $((a - b))"
+
+echo "Multiplication: $((8 * 5))"
+
+echo "Division: $((a / b))"
+
+echo "Modulus: $((a % b))"
+
+echo "Power: $((2 ** 8))"
